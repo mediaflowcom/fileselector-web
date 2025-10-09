079a7bb9514223e459fffe3716d02ba99f28ff95
Peraxel Märs <peraxel.mars@mediaflow.com>
2025-10-09 10:25:19 +0200

    .com

--- src/js/presets/env.example.js
+++ src/js/presets/env.example.js
@@ -2,5 +2,5 @@ export const credentials = {
     CLIENT_ID: 'xxxxxxxx',
     CLIENT_SECRET: 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',
     REFRESH_TOKEN: '7GKD4P9ZQ1M8VYC3N2TR5L0XB6SWAHFJ',
-    DOMAIN: 'mediaflow.tech'
+    DOMAIN: 'mediaflow.com'
 };
